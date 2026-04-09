28d676a71f38bdf7f8a1b0565fd8a8b94c83f943
Dispatch <dispatch@cartercodes.dev>
2026-04-08 21:12:47 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-heather-williams-licensed-realtor-ga-al-columbus',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
